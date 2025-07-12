46579ebc7ea47e5ccf2cebe05d9e05d36554be25
Arkadip <arkadip671@gmail.com>
2025-07-12 22:24:51 +0530

    changed footer description

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -12,8 +12,7 @@ const Footer = () => {
             <img src={footerLogo} alt="logo" width={150} height={46} />
           </a>
           <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
-            Get shoes ready for the new term at your nearest Nike store. Find
-            your perfect size in store. Get Rewards.
+            This is not an official Nike website. Just a fun project built by me. Thank you.
           </p>
           <div className="flex items-center gap-5 mt-8">
             {socialMedia.map((icon) => (
@@ -41,7 +40,7 @@ const Footer = () => {
       <div className="flex justify-between mt-24 text-white-400 max-sm:flex-col max-sm:items-center">
         <div className="flex flex-1 justify-start items-center font-montserrat cursor-pointer">
           <img src={copyrightSign} alt="copyright sign" width={20} height={20} className="rounded-full m-0"/>
-          <p>Copyright. All rights reserved.</p>
+          <p>Copyright. By Arkadip Das</p>
         </div>
         <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
       </div>
